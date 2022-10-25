651218ede5c6c0b74f00d799edca0677912024f8
Kateryna Solodilova <kiivikat@gmail.com>
2022-10-25 16:43:15 +0300

    finishes tasks 1,2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listEl = document.querySelector('#ingredients');
+
+const newItems = ingredients.map(ingredient => {
+  const itemEl = document.createElement('li');
+  itemEl.textContent = ingredient;
+  itemEl.classList.add('item');
+  return itemEl;
+});
+
+listEl.append(...newItems);
+
+console.log(listEl);
